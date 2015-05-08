407688ffee8e90336abf9dd5562e7757ed5f3e33
__aaronspring__ <spring@stud.uni-heidelberg.de>
2015-05-08 09:59:37 +0200

    android user class added

---
+++ chatter/AndroidUser.swift
@@ -0,0 +1,41 @@
+//
+//  AndroidUser.swift
+//  chatter
+//
+//  Created by Fischer, Nils on 08.05.15.
+//  Copyright (c) 2015 Universität Heidelberg. All rights reserved.
+//
+
+import UIKit
+
+class AndroidUser: Chatter {
+    
+    required init() {
+        super.init(image: UIImage(named: "homer"))
+    }
+    
+    override func nextMessage() -> Message {
+        return [
+            Message(content: "Story of my life.", type: .Statement),
+            Message(content: "I'm a cop from New York.", type: .Statement),
+            Message(content: "Drop the gun!", type: .Statement),
+            Message(content: "Who's driving this car, Stevie Wonder?", type: .QuestionBool),
+            Message(content: "Why'd you have to nuke the whole building, Hans? ", type: .QuestionWhy),
+            Message(content: "Welcome to the party, pal.", type: .Statement),
+            Message(content: "Yippee-ki-yay, motherf***er.", type: .Statement)
+            ].randomElement()
+    }
+    
+    override func responseForMessage(message: Message) -> Message {
+        return [
+            Message(content: "It's okay, I'm a cop.", type: .QuestionWhy),
+            Message(content: "It rings a bell, yeah.", type: .QuestionBool),
+            Message(content: "No, not since this morning.", type: .QuestionBool),
+            Message(content: "You tell me, kid. You're the criminal.", type: .QuestionWhy),
+            Message(content: "Yeah. That's what my captain keeps telling me.", type: .Statement),
+            Message(content: "Someone give me some aspirins.", type: .Joke)
+            ].randomElement()
+    }
+    
+    
+}
