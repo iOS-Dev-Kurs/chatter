4ded5e4c28412cf5590f8c2dc7bb88bb85f9b6da
Thomas Steinle <thomas@Thomass-Mac.local>
2016-05-02 01:58:41 +0200

    magic conch shell finished

--- chatter/Magic Conch Shell.swift
+++ chatter/Magic Conch Shell.swift
@@ -16,15 +16,48 @@ class MagicConchShell: Chatter {
     }
     
     override func nextMessage() -> Message {
-        return [
-            Message(content: "No.", type: .Statement),
-            Message(content: "Yes", type: .QuestionBool),
-            Message(content: "Maybe someday.", type: .Statement),
+    return [
+            Message(content: "Ask me a question.", type: .Statement),
+            Message(content: "", type: .Statement),
+            Message(content: "", type: .QuestionBool),
+            Message(content: "", type: .QuestionWhy),
             ].randomElement()
     }
     
     override func responseForMessage(message: Message) -> Message {
-        return Message(content: "No.", type: .Statement)
+        switch message.type {
+            
+        case .Statement:
+            return[
+                
+                Message(content: "No.", type: .Statement),
+                Message(content: "Yes.", type: .Statement),
+                Message(content: "I don't think so.", type: .Statement),
+                ].randomElement()
+            
+        case .QuestionBool:
+            return[
+                
+                Message(content: "No.", type: .Statement),
+                Message(content: "Yes.", type: .Statement),
+                Message(content: "Maybe someday.", type: .Statement),
+                Message(content: "Try asking again.", type: .Statement),
+                Message(content: "I don't think so.", type: .Statement),
+                ].randomElement()
+            
+        case .QuestionWhy:
+            return[
+                
+                Message(content: "Try asking again.", type: .Statement),
+                ].randomElement()
+            
+        case .Joke:
+            return[
+                
+                Message(content: "Try joking again.", type: .Joke),
+                Message(content: "No.", type: .Statement),
+                ].randomElement()
+        }
     }
     
 }
